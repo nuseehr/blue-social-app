531395a337572f6caf8a406987aeaa6b36ab6a76
Lee Sun Hong <nuseehr@naver.com>
2019-08-04 23:29:59 +0900

    Add Hooks and add AddPost to pass it to the child

--- src/components/Home/PostForm.js
+++ src/components/Home/PostForm.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-const PostForm = () => (
-  <form className="write-form">
-    <textarea
-      className="form-control input-lg"
-      placeholder="무슨 생각을 하고 계신가요?"
-      spellCheck="false"
-    ></textarea>
-    <button type="submit" className="btn btn-primary">
-      공유하기
-    </button>
-  </form>
-);
+const PostForm = props => {
+  const { onPostCommit } = props;
+  const [contents, setContents] = useState("");
+
+  console.log(onPostCommit);
+  return (
+    <form
+      className="write-form"
+      onSubmit={e => {
+        e.preventDefault();
+        onPostCommit(contents);
+        setContents("COMMIT!!!!");
+      }}
+    >
+      <textarea
+        className="form-control input-lg"
+        placeholder="무슨 생각을 하고 계신가요?"
+        spellCheck="false"
+        value={contents}
+        onChange={e => setContents(e.target.value)}
+      ></textarea>
+      <button type="submit" className="btn btn-primary">
+        공유하기
+      </button>
+    </form>
+  );
+};
 
 export default PostForm;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import Post from "./Post";
 import PostForm from "./PostForm";
 
-const Home = ({ posts }) => {
+const Home = ({ posts, onPostCommit }) => {
   const postList = posts.map(post => <Post key={post.seq} post={post} />);
   return (
     <div className="posts container">
-      <PostForm />
+      <PostForm onPostCommit={onPostCommit} />
       {postList}
     </div>
   );
